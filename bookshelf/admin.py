b42da483a2c9be3c359a01b806d9a71640f55f81
Akutchi <hugo.buffler@ecole.ensicaen.fr>
2024-12-12 11:19:35 +0100

    added zoom to images

--- bookshelf/admin.py
+++ bookshelf/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+from django.db.models import F
+
 from .models import Book, Author, Tag, Book_Author_Link, Book_Tag_Link
 
-admin.site.register (Book)
+@admin.register (Book)
+class MyModelAdmin(admin.ModelAdmin):
+    ordering = [F('Title').asc(nulls_last=True)]
+
+@admin.register (Author)
+class MyModelAdmin(admin.ModelAdmin):
+    ordering = [F('Surname').asc(nulls_last=True)]
+
 admin.site.register (Tag)
-admin.site.register (Author)
 admin.site.register (Book_Author_Link)
 admin.site.register (Book_Tag_Link)
