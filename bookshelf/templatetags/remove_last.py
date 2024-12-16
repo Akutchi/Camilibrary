2a60dc89f9030eb0d8ffce9adb14e7266546ed7d
Akutchi <hugo.buffler@ecole.ensicaen.fr>
2024-12-16 22:51:45 +0100

    fixed localhost:8000/?filter= when deselecting all

--- bookshelf/templatetags/remove_last.py
+++ bookshelf/templatetags/remove_last.py
@@ -4,6 +4,10 @@ register = template.Library()
 
 @register.filter
 def remove_last (query):
-    print(query)
+
     query_list = query.split ("=")[1].split ("+")
+
+    if len(query_list [:-2]) == 0:
+        return ""
+
     return "?filter="+"+".join (query_list [:-2])
