5515605d19be8048c6daea5f7bfe0d8b789fce30
Akutchi <hugo.buffler@ecole.ensicaen.fr>
2024-12-16 23:29:36 +0100

    some css fixed

--- bookshelf/templatetags/remove_last.py
+++ bookshelf/templatetags/remove_last.py
@@ -7,6 +7,8 @@ def remove_last (query):
 
     query_list = query.split ("=")[1].split ("+")
 
+    #  If query is of form www.camilibrary.fr/?filter=
+    #  This may happen when deselecting every tag without clicking on "effacer"
     if len(query_list [:-2]) == 0:
         return ""
 
