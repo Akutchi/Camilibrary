76ddfb1b5db715b113cf85586b88991c5b416898
Akutchi <hugo.buffler@ecole.ensicaen.fr>
2024-12-17 12:19:50 +0100

    changed from blurry ends to buttons for tag list

--- bookshelf/static/js/index.js
+++ bookshelf/static/js/index.js
@@ -1,29 +1,30 @@
-localStorage ["Tags"] = JSON.stringify({"active": []})
+function Move (event) {
 
-const Right_Blur = document.getElementById ("RightBlurDiv");
-Right_Blur.classList.toggle ("Right_Blur", true);
+    let dx = 0;
+    if (event.id == "LeftArrow") {
+        dx = -100;
 
-function ToogleBlurryEnd () {
+    } else if (event.id == "RightArrow") {
+        dx = 100;
+    }
 
     const TagList = document.getElementById ("TagList");
     const w = TagList.clientWidth;
     const over = TagList.scrollWidth - w;
 
-    const Left_Blur = document.getElementById ("LeftBlurDiv");
-    const Right_Blur = document.getElementById ("RightBlurDiv");
-
-
-    if(TagList.scrollLeft == over) {
-        Left_Blur.classList.toggle ("Left_Blur", true);
-        Right_Blur.classList.toggle ("Right_Blur", false);
-
-
-    } else if(TagList.scrollLeft == 0) {
-        Left_Blur.classList.toggle ("Left_Blur", false);
-        Right_Blur.classList.toggle ("Right_Blur", true);
+    if(TagList.scrollLeft+dx < over) {
+        TagList.scrollLeft += dx;
 
     } else {
-        Left_Blur.classList.toggle ("Left_Blur", true);
-        Right_Blur.classList.toggle ("Right_Blur", true);
+        TagList.scrollLeft = over;
+
+    }
+
+    if(TagList.scrollLeft+dx > 0) {
+        TagList.scrollLeft += dx;
+
+    } else {
+        TagList.scrollLeft = 0;
     }
 }
+
