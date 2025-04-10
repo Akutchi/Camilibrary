431ec7b3ff6a0016499a145350d77b121cf59644
Kaitlyn <kaitlyn.buffler@tutamail.com>
2025-04-10 11:06:01 +0200

    transitionned from dict to class

--- graph_model.py
+++ graph_model.py
@@ -2,6 +2,23 @@ import random as rnd
 import matplotlib.pyplot as plt
 
 
+class Node:
+    def __init__(self, name, coords, adjacents):
+        self.name = name
+
+        self.coords = coords
+        if coords == [-1, -1]:
+            self.coords = [
+                round(rnd.uniform(10, 50), 1),
+                round(rnd.uniform(10, 50), 1),
+            ]
+
+        self.adjacents = adjacents
+        if adjacents != []:
+            # n-1 to account for the starting line number that is 1 in the .data
+            self.adjacents = [int(n.strip("\n")) - 1 for n in adjacents]
+
+
 def Load_Data():
     graph = []
 
@@ -12,25 +29,10 @@ def Load_Data():
             node_data = line.split("  ")
             node_name = node_data[0]
             node_coords = [float(c) for c in node_data[1].split(" ")]
+            node_neighbours = node_data[2].split(" ") if len(node_data) == 3 else []
 
-            if node_coords == [-1, -1]:
-                node_coords = [
-                    round(rnd.uniform(10, 50), 1),
-                    round(rnd.uniform(10, 50), 1),
-                ]
-
-            # n-1 to account for the starting line number that is 1 in the .data
-            node_neighbours = (
-                [int(n.strip("\n")) - 1 for n in node_data[2].split(" ")]
-                if len(node_data) == 3
-                else []
-            )
-
-            graph.append({
-                "name": node_name,
-                "coords": node_coords,
-                "adjacents": node_neighbours,
-            })
+            node = Node(node_name, node_coords, node_neighbours)
+            graph.append(node)
             line = f.readline()
 
     return graph
@@ -38,20 +40,20 @@ def Load_Data():
 
 def Print_Graph(graph):
     for node in graph:
-        plt.text(node["coords"][0], node["coords"][1], node["name"])
-        plt.plot(node["coords"][0], node["coords"][1], "ko")
+        plt.text(node.coords[0], node.coords[1], node.name)
+        plt.plot(node.coords[0], node.coords[1], "ko")
 
     plt.show()
 
 
 def Get_Node_Potentiel_Crossing(graph, node_number, vertex_number):
-    first_degree_adjacent = graph[node_number]["adjacents"][vertex_number]
+    first_degree_adjacent = graph[node_number].adjacents[vertex_number]
 
     potential_crossings = []
-    for second_degree_adjacent in graph[first_degree_adjacent]["adjacents"]:
+    for second_degree_adjacent in graph[first_degree_adjacent].adjacents:
         vertex_list = []
 
-        for adj in graph[second_degree_adjacent]["adjacents"]:
+        for adj in graph[second_degree_adjacent].adjacents:
             vertex_list.append((
                 second_degree_adjacent,
                 adj,
